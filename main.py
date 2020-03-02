b53f02da0856f78536c1d680d99816bfb4927f2f
Sebastiano <48064277+ssebastianoo@users.noreply.github.com>
2020-03-02 14:53:32 +0100

    added get_env

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+from dotenv import load_dotenv
 
 bot = commands.Bot(command_prefix = "c-")
 bot.load_extension("jishaku")
@@ -13,4 +14,6 @@ for filename in os.listdir('./cogs'):
   if filename.endswith('.py'):
     bot.load_extension(f'cogs.{filename[:-3]}')
 
+load_dotenv(dotenv_path = ".env")
+token = os.environ.get('secret')
 bot.run(token)
