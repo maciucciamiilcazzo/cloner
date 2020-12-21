d9abccfa2f2e6498247eca546ae2125719e3e165
Sebastiano <48064277+ssebastianoo@users.noreply.github.com>
2020-12-21 11:06:45 +0100

    clone roles

--- cogs/cloner.py
+++ cogs/cloner.py
@@ -14,38 +14,38 @@ class Cloner(commands.Cog):
 		guild2 = ctx.guild
 
 		try:
-
 			guild1 = self.bot.get_guild(guild_id)
 
 		except:
-
 			return await ctx.send("Guild not found.")
 
 		for a in guild2.text_channels:
-
 			await a.delete()
 
 		for a in guild2.voice_channels:
-
 			await a.delete()
 
 		for a in guild2.categories:
-
+			await a.delete()
+			
+		for a in guild2.roles():
 			await a.delete()
 
 		for a in guild1.categories:
-
 			await guild2.create_category(name = a.name)
 
 		for a in guild1.text_channels:
-
 			cat = discord.utils.get(guild2.categories, name = a.category.name)
 			await guild2.create_text_channel(name = a.name, category = cat, position = a.position, topic = a.topic, slowmode_delay = a.slowmode_delay)
 
 		for a in guild1.voice_channels:
-
 			cat = discord.utils.get(guild2.categories, name = a.category.name)
 			await guild2.create_voice_channel(name = a.name, category = cat, position = a.position, user_limit = a.user_limit, bitrate = a.bitrate)
+		
+		roles = guild1.roles
+		roles.reverse()
+		for a in roles:
+                         await guild2.create_role(name=role.name, color=role.color, permissions=role.permissions)
 
 		await ctx.author.send("Done!")
 
